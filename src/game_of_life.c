87b4d42523a3397b49716c59cc83db923f39d4ac
Laurene Bari <laureneb@ev-d4.kzn.21-school.ru>
2023-03-19 12:49:53 +0300

    delete unused functions

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -7,14 +7,6 @@ void view(int a[HEIGHT][WIDTH]);
 int neighbors(int a[HEIGHT][WIDTH], int x, int y);
 void make_next_table(int a[HEIGHT][WIDTH], int b[HEIGHT][WIDTH]);
 void swap_tables(int a[HEIGHT][WIDTH], int b[HEIGHT][WIDTH]);
-void glider(int a[HEIGHT][WIDTH]);
-void bee_hive(int a[HEIGHT][WIDTH]);
-void gosper_glider_gun (int a[HEIGHT][WIDTH]);
-void view_bin_table(int a[HEIGHT][WIDTH]);
-void loaf(int a[HEIGHT][WIDTH]);
-void pulsar(int a[HEIGHT][WIDTH]);
-void gosper_glider_gun(int a[HEIGHT][WIDTH]);
-void hwss(int a[HEIGHT][WIDTH]);
 void start_pic_choice(int a[HEIGHT][WIDTH]);
 void read_file(char *name, int a[HEIGHT][WIDTH]);
 int cnt_alive(int a[HEIGHT][WIDTH]);
@@ -108,15 +100,6 @@ void read_file(char *name, int a[HEIGHT][WIDTH]) {
     }            
 }
  
-void view_bin_table (int a[HEIGHT][WIDTH]) {
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            printf("%d", a[i][j]);
-        }
-        printf("\n");
-    }
-}
- 
 void view (int a[HEIGHT][WIDTH]) {
     for (int i = -1; i < HEIGHT + 1; i++) {
         for (int j = -1; j < WIDTH + 1; j++) {
@@ -171,137 +154,4 @@ void swap_tables(int a[HEIGHT][WIDTH], int b[HEIGHT][WIDTH]) {
             a[i][j] = b[i][j];
         }
     }
-}
-
-void glider(int a[HEIGHT][WIDTH]) {
-    a[1][1] = 1;
-    a[1][3] = 1;
-    a[2][2] = 1;
-    a[2][3] = 1;
-    a[3][2] = 1;
-}
-
-void bee_hive(int a[HEIGHT][WIDTH]) {
-    a[1][2] = 1;
-    a[1][3] = 1;
-    a[2][1] = 1;
-    a[2][4] = 1;
-    a[3][2] = 1;
-    a[3][3] = 1;
-}
-
-void gosper_glider_gun (int a[HEIGHT][WIDTH]) {
-    a[5][1] = 1;
-    a[6][1] = 1;
-    a[5][2] = 1;
-    a[6][2] = 1;
-    a[5][11] = 1;
-    a[6][11] = 1;
-    a[7][11] = 1;
-    a[4][12] = 1;
-    a[8][12] = 1;
-    a[3][13] = 1;
-    a[9][13] = 1;
-    a[3][14] = 1;
-    a[9][14] = 1;
-    a[6][15] = 1;
-    a[4][16] = 1;
-    a[8][16] = 1;
-    a[5][17] = 1;
-    a[6][17] = 1;
-    a[7][17] = 1;
-    a[6][18] = 1;
-    a[3][21] = 1;
-    a[4][21] = 1;
-    a[5][21] = 1;
-    a[3][22] = 1;
-    a[4][22] = 1;
-    a[5][22] = 1;
-    a[2][23] = 1;
-    a[6][23] = 1;
-    a[1][25] = 1;
-    a[2][25] = 1;
-    a[6][25] = 1;
-    a[7][25] = 1;
-    a[3][35] = 1;
-    a[4][35] = 1;
-    a[3][36] = 1;
-    a[4][36] = 1;
-}
-
-void loaf(int a[HEIGHT][WIDTH]) {
-    a[2][1] = 1;
-    a[1][2] = 1;
-    a[3][2] = 1;
-    a[1][3] = 1;
-    a[4][3] = 1;
-    a[3][4] = 1;
-    a[2][4] = 1;
-}
-
-void pulsar(int a[HEIGHT][WIDTH]) {
-    a[4][2] = 1;
-    a[5][2] = 1;
-    a[6][2] = 1;
-    a[10][2] = 1;
-    a[11][2] = 1;
-    a[12][2] = 1;
-    a[2][4] = 1;
-    a[2][5] = 1;
-    a[2][6] = 1;
-    a[2][10] = 1;
-    a[2][11] = 1;
-    a[2][12] = 1;
-    a[4][14] = 1;
-    a[5][14] = 1;
-    a[6][14] = 1;
-    a[10][14] = 1;
-    a[11][14] = 1;
-    a[12][14] = 1;
-    a[10][7] = 1;
-    a[11][7] = 1;
-    a[12][7] = 1;
-    a[10][9] = 1;
-    a[11][9] = 1;
-    a[12][9] = 1;
-    a[4][7] = 1;
-    a[5][7] = 1;
-    a[6][7] = 1;
-    a[4][9] = 1;
-    a[5][9] = 1;
-    a[6][9] = 1;
-    a[7][4] = 1;
-    a[7][5] = 1;
-    a[7][6] = 1;
-    a[7][10] = 1;
-    a[7][11] = 1;
-    a[7][12] = 1;
-    a[9][4] = 1;
-    a[9][5] = 1;
-    a[9][6] = 1;
-    a[9][10] = 1;
-    a[9][11] = 1;
-    a[9][12] = 1;
-    a[14][4] = 1;
-    a[14][5] = 1;
-    a[14][6] = 1;
-    a[14][10] = 1;
-    a[14][11] = 1;
-    a[14][12] = 1;
-}
-
-void hwss(int a[HEIGHT][WIDTH]) {
-    a[1][4] = 1;
-    a[1][5] = 1;
-    a[2][2] = 1;
-    a[2][7] = 1;
-    a[3][1] = 1;
-    a[4][1] = 1;
-    a[4][7] = 1;
-    a[5][1] = 1;
-    a[5][2] = 1;
-    a[5][3] = 1;
-    a[5][4] = 1;
-    a[5][5] = 1;
-    a[5][6] = 1;
 }
